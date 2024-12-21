e7358f93ed453de1740266c4cdd6727a9512c19e
343GAUTAMVIJAY <147785180+343GAUTAMVIJAY@users.noreply.github.com>
2024-12-21 18:12:59 +0530

    Create Stdin and Stdout I.java

---
+++ 01.01.2025/Stdin and Stdout I.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Reading three integers from the user
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        // Printing the integers
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        
+        scan.close(); // Close the scanner
+    }
+}
